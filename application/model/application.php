acb6dcd50cd53e0d44fd7911c0440fea54248f69
Reka <reka37@mail.ru>
2019-11-09 10:37:22 +0300

    try catch

--- application/model/application.php
+++ application/model/application.php
@@ -27,21 +27,32 @@ class ApplicationModel extends ParentApplicationModel
 		
     public function __construct($dbh, $name, $autor, $udk, $bbk, $date, $place, $anotation) 
     {
-        $query = "INSERT INTO `books` (`name`, `autor`, `udk`, `bbk`, `date`, `place`, `anotation`) VALUES (:name, :autor, :udk, :bbk, :date, :place, :anotation)";
-        $stmt = $dbh->prepare($query);
-        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-        $stmt->bindValue(':autor', $autor, PDO::PARAM_STR);
-        $stmt->bindValue(':udk', $udk, PDO::PARAM_STR);
-        $stmt->bindValue(':bbk', $bbk, PDO::PARAM_STR);
-        $stmt->bindValue(':date', $date, PDO::PARAM_STR);
-        $stmt->bindValue(':place', $place, PDO::PARAM_STR);
-        $stmt->bindValue(':anotation', $anotation, PDO::PARAM_STR);
-        $res = $stmt->execute();
-        if ($res) {
-            $this->result = true;
-        } else {
-            $this->result = false; 
-        }
+        try {
+            $dbh->beginTransaction();
+            $query = "INSERT INTO `books` "
+                        . "(`name`, `autor`, `udk`, `bbk`, `date`, `place`, `anotation`) "
+                        . "VALUES "
+                        . "(:name, :autor, :udk, :bbk, :date, :place, :anotation)";
+            $stmt = $dbh->prepare($query);
+            $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+            $stmt->bindValue(':autor', $autor, PDO::PARAM_STR);
+            $stmt->bindValue(':udk', $udk, PDO::PARAM_STR);
+            $stmt->bindValue(':bbk', $bbk, PDO::PARAM_STR);
+            $stmt->bindValue(':date', $date, PDO::PARAM_STR);
+            $stmt->bindValue(':place', $place, PDO::PARAM_STR);
+            $stmt->bindValue(':anotation', $anotation, PDO::PARAM_STR);
+            $res = $stmt->execute();
+            $dbh->commit();
+            
+            if ($res) {
+                $this->result = true;
+            } else {
+                $this->result = false; 
+            }           
+        } catch( PDOException $e ) {
+            $dbh->rollback();
+            $this->result = false;
+        }    
     }
 
     /**
@@ -60,6 +71,7 @@ if (!empty($_POST['submit'])) {
     require_once '../config.php';
     $autor = implode(',', $_POST['autor']);
     $applicationModel = new ApplicationModel($dbh, $_POST['name'], $autor, $_POST['udk'], $_POST['bbk'], $_POST['date'], $_POST['place'], $_POST['anotation']);
+    
     if ($applicationModel->answer()) {
             header('Location: ../../index.php?result=true');
     } else {
